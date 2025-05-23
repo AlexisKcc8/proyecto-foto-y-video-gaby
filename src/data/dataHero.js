6739bf730a8198aa9d9fa5e852f6b5314de7ef48
AlexisKcc8 <CodeKJS863@gmail.com>
2025-05-23 09:11:08 -0600

    feat: add ContainerCenter component and refactor sections
    - Implemented ContainerCenter component for consistent layout.
    - Refactored AboutMe, BannerMessage, and Hero sections to utilize ContainerCenter.
    - Created Button component with variants for styling.
    - Added utility function for class name merging.
    - Introduced global styles and typography settings.
    - Integrated background images for Hero section from data file.

---
+++ src/data/dataHero.js
@@ -0,0 +1,17 @@
+import misXV from "@assets/hero/mis-xv.jpg";
+import señoritaHalacho from "@assets/hero/señorita-halacho.jpg";
+import boda from "@assets/hero/boda.jpg";
+export const backgroundsHero = [
+  {
+    id: crypto.randomUUID(),
+    urlImage: misXV,
+  },
+  {
+    id: crypto.randomUUID(),
+    urlImage: señoritaHalacho,
+  },
+  {
+    id: crypto.randomUUID(),
+    urlImage: boda,
+  },
+];
