0a60d0785deb41b6c476aad623d9560a05c3588c
AlexisKcc8 <CodeKJS863@gmail.com>
2025-05-29 13:08:38 -0600

    feat: add testimonials data and update Testimonials component to utilize dynamic content

---
+++ src/data/dataTestimonial.js
@@ -0,0 +1,80 @@
+export const testimonials = [
+  {
+    quote:
+      "Hicimos fotos para nuestro restaurante y ¡quedaron espectaculares! Gaby entendió justo lo que queríamos mostrar: sabor y tradición.",
+    name: "Emilio Chuc",
+    initial: "E",
+    bgColor: "bg-indigo-100",
+    textColor: "text-indigo-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "Muy profesionales, puntuales y con excelente calidad. Hicimos una sesión familiar y quedaron recuerdos hermosos que ahora decoran nuestra casa. Los recomiendo al 100%.",
+    name: "Carlos Hernández",
+    initial: "C",
+    bgColor: "bg-blue-100",
+    textColor: "text-blue-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "¡Increíble experiencia! Me sentí muy cómoda durante la sesión de XV años y las fotos quedaron espectaculares. Gracias Gaby por tu paciencia y talento.",
+    name: "Mariana Gutiérrez",
+    initial: "M",
+    bgColor: "bg-purple-100",
+    textColor: "text-purple-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "Desde la sesión previa a la boda hasta el último baile, capturaron todo con un ojo artístico impresionante. ¡Nos hicieron revivir nuestro gran día con cada imagen!",
+    name: "David y Sofía",
+    initial: "D",
+    bgColor: "bg-green-100",
+    textColor: "text-green-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "Las fotos de los 3 años de mi hija quedaron preciosas. Todo el equipo fue muy paciente y cariñoso con ella. ¡Repetiremos sin duda!",
+    name: "Marisol Pech",
+    initial: "M",
+    bgColor: "bg-yellow-100",
+    textColor: "text-yellow-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "Organizamos una sesión para mis abuelitos y fue una experiencia única. Lograron capturar su historia de amor como nunca imaginamos.",
+    name: "Julián Chan",
+    initial: "J",
+    bgColor: "bg-red-100",
+    textColor: "text-red-600",
+    mobileOnly: true,
+  },
+  {
+    quote:
+      "Nos encantó el profesionalismo y el trato tan cálido. La sesión de embarazo fue tranquila, divertida y llena de momentos lindos.",
+    name: "Araceli Dzib",
+    initial: "A",
+    bgColor: "bg-teal-100",
+    textColor: "text-teal-600",
+  },
+  {
+    quote:
+      "Increíble atención desde el primer mensaje. Todo el equipo fue muy amable y profesional. Las fotos tienen un estilo muy natural y bonito.",
+    name: "Esteban Chi",
+    initial: "E",
+    bgColor: "bg-red-100",
+    textColor: "text-red-600",
+  },
+  {
+    quote:
+      "Contratamos a Gaby para una sesión con nuestro grupo de danza folclórica. ¡Quedaron espectaculares las tomas con los trajes típicos!",
+    name: "María del Carmen Tuz",
+    initial: "M",
+    bgColor: "bg-teal-100",
+    textColor: "text-teal-600",
+  },
+];
